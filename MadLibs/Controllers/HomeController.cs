33f9c7e5e8618c0b84f66ba5748646616d562930
Levi Kohler <kohler.la01@gmail.com>
2021-03-01 14:01:07 -0800

    change naming of MadLibs to more specific ComputerMadLibs which allows us to have more than 1 MadLibs which will all be linked on the newly created HomePage
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,21 +6,24 @@ namespace MadLibs.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult Form() {return View();}
+    public ActionResult HomePage() {return View();}
 
-    [Route("/madlib")]
-      public ActionResult MadLib(string noun, string pluralNoun, string presentTenseVerb, string presentTenseVerbTwo, string partOfBody, string adjective, string pluralNounTwo, string adjectiveTwo)
+    [Route("/computerform")]
+    public ActionResult ComputerForm() {return View();}
+
+    [Route("/computermadlib")]
+      public ActionResult ComputerMadLib(string noun, string pluralNoun, string presentTenseVerb, string presentTenseVerbTwo, string partOfBody, string adjective, string pluralNounTwo, string adjectiveTwo)
       {
-        MadLibsVariable myMadLibsVariable = new MadLibsVariable();
-        myMadLibsVariable.Noun = noun;
-        myMadLibsVariable.PluralNoun = pluralNoun;
-        myMadLibsVariable.PresentTenseVerb = presentTenseVerb;
-        myMadLibsVariable.PresentTenseVerbTwo = presentTenseVerbTwo;
-        myMadLibsVariable.PartOfBody = partOfBody;
-        myMadLibsVariable.Adjective = adjective;
-        myMadLibsVariable.PluralNounTwo = pluralNounTwo;
-        myMadLibsVariable.AdjectiveTwo = adjectiveTwo;
-        return View(myMadLibsVariable);
+        ComputerVariable myComputerVariable = new ComputerVariable();
+        myComputerVariable.Noun = noun;
+        myComputerVariable.PluralNoun = pluralNoun;
+        myComputerVariable.PresentTenseVerb = presentTenseVerb;
+        myComputerVariable.PresentTenseVerbTwo = presentTenseVerbTwo;
+        myComputerVariable.PartOfBody = partOfBody;
+        myComputerVariable.Adjective = adjective;
+        myComputerVariable.PluralNounTwo = pluralNounTwo;
+        myComputerVariable.AdjectiveTwo = adjectiveTwo;
+        return View(myComputerVariable);
       }
   }
 }
--- MadLibs/Models/MadLibsVariable.cs
+++ MadLibs/Models/MadLibsVariable.cs
@@ -1,6 +1,6 @@
 namespace MadLibs.Models
 {
-  public class MadLibsVariable
+  public class ComputerVariable
   {
     public string Noun { get; set; }
     public string PluralNoun { get; set; }
@@ -10,5 +10,10 @@ namespace MadLibs.Models
     public string Adjective { get; set; }
     public string PluralNounTwo { get; set; }
     public string AdjectiveTwo { get; set; }
+  }
+
+  public class DJVariable
+  {
+
   }
 } 
